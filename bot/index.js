ddd0cdb356cc5d38ada07f4b1365b6244046ed11
Abolaji <guppyzey@gmail.com>
2025-02-11 04:45:40 +0100

    Bot Troubleshooting

--- bot/index.js
+++ bot/index.js
@@ -1,19 +1,19 @@
-const TelegramBot = require('node-telegram-bot-api');
+/* const TelegramBot = require('node-telegram-bot-api');
 const axios =require('axios');
 const express = require('express');
 require('dotenv').config();
 
 //Token gotten from BotFather
-const token = process.env.Telegram_Token;
+const token = process.env.Telegram_Token; */
 /* const token = '8109321488:AAH5bd7bxVTSz6__HugRn0F02BlODujC9Pc'; */
-
+/*
 const bot = new TelegramBot(token, { polling: true });
 
 const keyboards = require('./keyboards')(bot);
 const utils = require('./utils')(bot, keyboards);
 const handlers = require('./handlers')(bot, keyboards, utils);
 
-bot.on('message', (msg) => {
+ bot.on('message', (msg) => {
     console.log('Received message:', msg);
     handlers.messageHandler(msg);
 });
@@ -21,17 +21,17 @@ bot.on('message', (msg) => {
 bot.on('callback_query', (query) => {
     console.log('Received callback query:', query);
     handlers.callbackQueryHandler(query);
-});
+}); 
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static('public')); */
 
 /*app.get('/', (req, res) => {
     res.send('Kindly join our Telegram channels and subscribe to our YouTube channels.');
 });*/
-
+/*
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
@@ -48,4 +48,20 @@ bot.on('error', (error) => {
             bot.startPolling();
         }, 1000);
     }
+});
+*/
+
+const TelegramBot = require('node-telegram-bot-api');
+const token = '8109321488:AAH5bd7bxVTSz6__HugRn0F02BlODujC9Pcbot';
+
+const bot = new TelegramBot(token, {polling: true});
+
+bot.on('message', (msg) => {
+    if (msg.text === '/start') {
+        bot.sendMessage(msg.chat.id, 'Welcome!');
+    }
+});
+
+bot.on('error', (error) => {
+    console.error(error);
 });
