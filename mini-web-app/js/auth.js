4da1bdabb72e99f4581eb5f52954e0d8ffc90b1f
Abolaji <guppyzey@gmail.com>
2025-02-12 11:30:37 +0100

    Updated js files

--- mini-web-app/js/auth.js
+++ mini-web-app/js/auth.js
@@ -1,10 +1,10 @@
-
+const crypto = require('crypto');
 function verifyTelegramAuth(telegramAuth) {
   const telegramAuthHash = crypto.createHash('sha256');
   telegramAuthHash.update(process.env.TELEGRAM_AUTH_HASH_SECRET);
   const hash = telegramAuthHash.digest('hex');
 
-  return hash === telegramAuth;
+  return crypto.timingSafeEqual(Buffer.from(hash), Buffer.from(telegramAuth));
 }
 
 module.exports = verifyTelegramAuth;
--- mini-web-app/js/server.js
+++ mini-web-app/js/server.js
@@ -4,19 +4,35 @@ const app = express();
 const path = require('path');
 const verifyTelegramAuth = require('./auth'); // Import the verifyTelegramAuth function
 
-/* app.use(express.static(path.join(__dirname, '../mini-web-app'))); */
-app.use(express.static(path.join(__dirname, '../')));
-
-app.get('/', (req, res) => {
+// Authentication middleware
+const authenticate = (req, res, next) => {
   const telegramAuth = req.query['telegram-auth'];
   if (verifyTelegramAuth(telegramAuth)) {
-    // Serve the index.html file
-    res.sendFile(path.join(__dirname, '../index.html'));
+    next();
   } else {
     res.status(401).send('Unauthorized');
   }
-});
+};
 
+// Use authentication middleware for all routes
+app.use(authenticate);
+
+// Serve static files
+app.use(express.static(path.join(__dirname, '../mini-web-app')));
+/*
+app.get('/', (req, res) => {
+  console.log('Request query:', req.query);
+  const telegramAuth = req.query['telegram-auth'];
+  if (verifyTelegramAuth(telegramAuth)) {
+    console.log('Authentication successful');
+    // Serve the index.html file
+    res.sendFile(path.join(__dirname, '../index.html'));
+  } else {
+    console.log('Authentication failed');
+    res.status(401).send('Unauthorized');
+  }
+}); 
+*/
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log('Server listening on port 3000');
